d2e4e0485053c6c32740d5ba5e6780fba5db2684
ramaprakoso <rama.wiedprakoso@gmail.com>
2016-11-07 01:13:09 +0700

    riset featue extarction svm

---
+++ riset.py
@@ -0,0 +1,203 @@
+#import regex
+import re
+import csv
+import pprint
+import nltk.classify
+
+#initialize stopWords
+stopWords = []
+
+#start replaceTwoOrMore
+def replaceTwoOrMore(s):
+    #look for 2 or more repetitions of character and replace with the character itself
+    pattern = re.compile(r"(.)\1{1,}", re.DOTALL)
+    return pattern.sub(r"\1\1", s)
+#end
+def processTweet(tweet):
+    # process the tweets
+    #Convert to lower case
+    tweet = tweet.lower()
+    #Convert www.* or https?://* to URL
+    tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','URL',tweet)
+    #Convert @username to AT_USER
+    tweet = re.sub('@[^\s]+','',tweet)    
+    #Remove additional white spaces
+    tweet = re.sub('[\s]+', ' ', tweet)
+    #Replace #word with word
+    tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+    #trim
+    tweet = tweet.strip('\'"')
+
+    return tweet 
+#start getStopWordList
+def getStopWordList(stopWordListFileName):
+    #read the stopwords file and build a list
+    stopWords = []
+    stopWords.append('AT_USER')
+    stopWords.append('URL')
+
+    fp = open(stopWordListFileName, 'r')
+    line = fp.readline()
+    while line:
+        word = line.strip()
+        stopWords.append(word)
+        line = fp.readline()
+    fp.close()
+    return stopWords
+#end
+
+#start getfeatureVector
+def getFeatureVector(tweet,stopWords):
+    featureVector = []
+    #split tweet into words
+    words = tweet.split()
+    for w in words:
+        #replace two or more with two occurrences
+        w = replaceTwoOrMore(w)
+        #strip punctuation
+        w = w.strip('\'"?,.')
+        #check if the word stats with an alphabet
+        val = re.search(r"^[a-zA-Z][a-zA-Z0-9]*$", w)
+        #ignore if it is a stop word
+        if(w in stopWords or val is None):
+            continue
+        else:
+            featureVector.append(w.lower())
+    return featureVector
+#end
+def extract_features(tweet):
+    features = {}
+    for word in featureList:
+        features['fitur(%s)' % word] = (word in tweet)
+    return features
+def getSVMFeatureVectorAndLabels(tweets, featureList):
+    sortedFeatures = sorted(featureList)
+    map = {}
+    feature_vector = []
+    labels = []
+    for t in tweets:
+        label = 0
+        map = {}
+        #Initialize empty map
+        for w in sortedFeatures:
+            map[w] = 0
+
+        tweet_words = t[0]
+        tweet_opinion = t[1]
+        #Fill the map
+        for word in tweet_words:
+            #process the word (remove repetitions and punctuations)
+            word = replaceTwoOrMore(word)
+            word = word.strip('\'"?,.')
+            #set map[word] to 1 if word exists
+            if word in map:
+                map[word] = 1
+        #end for loop
+        values = map.values()
+        feature_vector.append(values)
+        if(tweet_opinion == 'positive'):
+            label = 0
+        elif(tweet_opinion == 'negative'):
+            label = 1
+        elif(tweet_opinion == 'neutral'):
+            label = 2
+        labels.append(label)
+    #return the list of feature_vector and labels
+    return {'feature_vector' : feature_vector, 'labels': labels}
+#end
+#end
+#Read the tweets one by one and process it
+fp = open('twiit.csv', 'r')
+line = fp.readline()
+
+st = open('stopword.txt', 'r')
+stopWords = getStopWordList('stopword.txt')
+inpTweets = csv.reader(open('twiit.csv', 'rb'), delimiter=';', quotechar='|')
+count=0
+tweets = []
+featureList= []
+for row in inpTweets:
+    tweet = row[0]
+    sentiment = row[1]
+    processedTweet = processTweet(tweet)
+    featureVector = getFeatureVector(processedTweet, stopWords)
+    featureList.extend(featureVector)
+    tweets.append((featureVector, sentiment))
+	
+	
+featureList = list(set(featureList))
+print getSVMFeatureVectorAndLabels(tweets,featureList)
+
+
+"""
+while line:
+    processedTweet = processTweet(line)
+    featureVector = getFeatureVector(processedTweet)
+    print featureVector
+    line = fp.readline()
+#end loop
+"""
+fp.close()
+"""
+def replaceTwoOrMore(s):
+    #look for 2 or more repetitions of character and replace with the character itself
+    pattern = re.compile(r"(.)\1{1,}", re.DOTALL)
+    return pattern.sub(r"\1\1", s)
+#start process_tweet
+def processTweet(tweet):
+    # process the tweets
+    #Convert to lower case
+    tweet = tweet.lower()
+    #Convert www.* or https?://* to URL
+    tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','URL',tweet)
+    #Convert @username to AT_USER
+    tweet = re.sub('@[^\s]+','',tweet)    
+    #Remove additional white spaces
+    tweet = re.sub('[\s]+', ' ', tweet)
+    #Replace #word with word
+    tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+    #trim
+    tweet = tweet.strip('\'"')
+
+    return tweet 
+def getFeatureVector(tweet): 
+		featureVector = []
+		#split tweet into words
+		words = tweet.split()
+		for w in words:
+			#strip punctuation
+			w = w.strip('\'"?,.')
+			#check if it consists of only words
+			val = re.search(r"^[a-zA-Z][a-zA-Z0-9]*[a-zA-Z]+[a-zA-Z0-9]*$", w)
+			#ignore if it is a stopWord
+			if(w in stopWords or val is None):
+				continue
+			else:
+				featureVector.append(w.lower())
+		return featureVector    
+#get stopwords 
+def getStopWordList(filename): 
+	stopWords=[]
+	fp=open('stopword.txt','r')
+	line=fp.readline()
+	while line: 
+		word=line.strip()
+		stopWords.append(word)
+		line=fp.readline()
+	fp.close()
+	return stopWords
+
+	tr=open('twiit.csv','r')
+	line=tr.readline()
+	print line 
+	st=open('stopword.txt','r')
+	stopword=getStopWordList(st)
+	while line:
+		processedTweet = processTweet(line)
+		featureVector = getFeatureVector(processedTweet)
+		#print featureVector
+		#print featureVector 
+		line = fp.readline()
+	fp.close()
+"""
+		

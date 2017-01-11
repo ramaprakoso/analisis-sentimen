1e9b30c47e7fef8a046efa11058fd407eefca8e2
ramaprakoso <rama.wiedprakoso@gmail.com>
2017-01-12 02:19:46 +0700

    update

--- svm.py
+++ svm.py
@@ -1,5 +1,9 @@
 import preprocessing 
 import pandas as pd
+import csv
+from sklearn import svm 
+from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import svm 
 from sklearn.metrics import accuracy_score
 
@@ -8,16 +12,29 @@ fp=open('dataset/train_fitur.csv','r')
 line=fp.read()
 pre=preprocessing.preprocess(line)
 fitur=preprocessing.get_fitur(pre)
-print fitur 
-#print get_fitur 
-#train_df = pandas.read_csv('train.csv',sep=',',dtype={'label':int, 'tweet':str})
 
+train_df_raw = pd.read_csv('dataset/train.csv',sep=';',names=['tweets','label'],header=None)
+test_df_raw = pd.read_csv('dataset/testing.csv',sep=';',names=['tweets','label'],header=None)
+train_df_raw = train_df_raw[train_df_raw['tweets'].notnull()]
+test_df_raw = test_df_raw[test_df_raw['tweets'].notnull()]
 
+#ekstrak make training and testing 
+docs_train=train_df_raw['tweets'].tolist()
+docs_test=test_df_raw['tweets'].tolist()
+y_train=[x if x=='positif' else 'negatif' for x in train_df_raw['label'].tolist()]
+y_test=[x if x=='positif' else 'negatif' for x in test_df_raw['label'].tolist()]
 
+vectorizer = TfidfVectorizer(max_df=1.0, max_features=10000,
+                             min_df=0, vocabulary=fitur)
+vectorizer.fit(docs_test+docs_train)
+X_train=vectorizer.transform(docs_train)
+X_test=vectorizer.transform(docs_test)
 
+clf=svm.SVC(max_iter=1000)
+clf.fit(X_train,y_train)
 
-
-
+prediction=clf.predict(X_test)
+print 'accuracy : ',accuracy_score(y_test,prediction)                    
 """
 KERNEL_FUNCTION="linear"
 C_PARAMETER=0.6
